5b510042e1ed557802ce572073ec2b46863f7a25
Selasi3 <selasiayittah3@gmail.com>
2022-10-27 10:11:19 +0000

    added Music , Artist and Lyric models

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Song, Artist, Lyric
 # Register your models here.
+
+admin.site.register(Song)
+admin.site.register(Artist)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=18)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField()
+    artist = models.ForeignKey("Artist",  on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Lyric(models.Model):
+    content = models.CharField(max_length=500)
+    song = models.ForeignKey("Song", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.song.title
+    
